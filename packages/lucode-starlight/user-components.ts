71dea4687d3efd8d05a8537fa7595a6404433063
Lucas Colombo <lucasncolombo@gmail.com>
2026-04-26 18:29:49 -0300

    feat: ✨ add dropdown component

--- packages/lucode-starlight/user-components.ts
+++ packages/lucode-starlight/user-components.ts
@@ -1,6 +1,3 @@
-// export { default as Callout } from './components/custom/Callout.astro'
 export { default as ContainerSection } from './components/custom/ContainerSection.astro';
 export { default as LinkButton } from './components/custom/LinkButton.astro';
-// export { default as LinkedCard } from './components/custom/LinkedCard.astro'
-// export { default as LinkedCardGroup } from './components/custom/LinkedCardGroup.astro'
-// export { default as Steps } from './components/custom/Steps.astro'
+export { default as Dropdown } from './components/custom/dropdown';
