1203515d3f57e2370d71d6581ad6a2c0ab347a4b
Lucas Colombo <lucasncolombo@gmail.com>
2026-04-26 18:34:54 -0300

    feat: ✨ enhance Footer component with LastUpdated, EditLink, and Pagination

--- packages/lucode-starlight/virtual.d.ts
+++ packages/lucode-starlight/virtual.d.ts
@@ -34,3 +34,18 @@ declare module 'virtual:starlight/project-context' {
     };
     export default ProjectContext;
 }
+
+declare module 'virtual:starlight/components/LastUpdated' {
+    const LastUpdated: typeof import('@astrojs/starlight/components/LastUpdated.astro').default;
+    export default LastUpdated;
+}
+
+declare module 'virtual:starlight/components/Pagination' {
+    const Pagination: typeof import('@astrojs/starlight/components/Pagination.astro').default;
+    export default Pagination;
+}
+
+declare module 'virtual:starlight/components/EditLink' {
+    const EditLink: typeof import('@astrojs/starlight/components/EditLink.astro').default;
+    export default EditLink;
+}
